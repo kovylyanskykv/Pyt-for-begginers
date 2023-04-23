3ee59d39ad8630437dd54ad1ad6ed79b6150a301
kovylyanskykv <uanlu@outlook.com>
2023-04-23 23:13:58 +0300

    Убрал лишние строки.

--- HW-2.py
+++ HW-2.py
@@ -4,4 +4,4 @@ sum_num1 = int(num[0:1])
 sum_num2 = int(num[1:2])
 sum_num3 = int(num[2:3])
 sum = sum_num1 + sum_num2 + sum_num3
-print(str(num) + " >>> Сумма числа " + str(num) + " равна " + str(sum))
+print(num + " >>> Сумма числа " + num + " равна " + str(sum))
