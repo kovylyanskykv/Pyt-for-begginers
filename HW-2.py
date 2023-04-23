3d43d405ccfd2199b39620d0cd40bd801f94c74b
kovylyanskykv <uanlu@outlook.com>
2023-04-23 16:27:14 +0300

    Подготовил фа

--- HW-2.py
+++ HW-2.py
@@ -1,3 +1,4 @@
+
 num = str(input("Введите число:"))
 sum_num1 = int(num[0:1])
 sum_num2 = int(num[1:2])
@@ -6,5 +7,3 @@ sum = sum_num1 + sum_num2 + sum_num3
 print(str(num) + " >>> Сумма числа " + str(num) + " равна " + str(sum))
 
 
-#Могу ли я преобразовывать sum_num в int?
-# sum = str(int(num[1:1]) + int(num[1:2]) + int(num[1:3]))
